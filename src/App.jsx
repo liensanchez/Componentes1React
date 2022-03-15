9b86c041cc22a40255b3712a915cd83bba0ed983
Lien Sanchez <liensilviosanchez@gmail.com>
2022-03-15 12:21:43 -0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Titulo from './components/Titulo/Titulo';
 
+<></> /* se hace esto para evitar hacer un div y que nos haga otro nodo */
 
+//componente Contenedor=app
 
 function App() {
+
+  let titulo='soy titulo dinamico' //las variables son estados, solo presentan datos
+  let subtitulo='subtitulo hola'
 
   return (
 
@@ -13,8 +18,8 @@ function App() {
 
       <NavBar />
 
-      <Titulo/>
-
+      <Titulo tituloProps={titulo} subtituloProps={subtitulo} /> {/*Extremadamente modular y dinamico porque directamente lo hice una sola vez */}
+      {/*tmb se puede poner como Titulo()*/}
 
     </div>
   );
